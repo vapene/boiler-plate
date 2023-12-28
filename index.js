b23dd331ec2459feb8e20c24aeda944b8687d994
vapene <parkjjong@hotmail.com>
2023-12-28 18:17:50 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -4,52 +4,37 @@ const port = 3000
 const bodyParser = require('body-parser');
 
 const config = require('./config/key')
-const { User } = require("./models/User");
 
+const {User} = require('./models/User');
 
 app.use(bodyParser.urlencoded({extended:true}));
-
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect(config.mongoURI,{
-    useNewUrlParser: true, useUnifiedTopology: true
-}).then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
+mongoose.connect(config.mongoURI). then( () => console.log('mongoDB connected..')).catch(err => console.log(err))
 
-const Schema = mongoose.Schema;
 
-const productSchema = mongoose.Schema({
-    writer:{
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    title: {
-        type: String,
-        maxlenght:50
-    },
-    description:{
-        type: String
-    }
-}, {timestamps: true});
 
-const Product = mongoose.model('Product', productSchema);
-//module.exports= { Product }
+app.get('/', (req, res) => res.send('Heelow worlds'))
 
-app.get('/', (req, res) => res.send('Hello world'))
-
-app.post('/register', async (req, res) => {
-    const user = new User(req.body);
-
-    try {
-        await user.save();
-        res.status(200).json({ success: true });
-    } catch (err) {
+app.post('/register', async (req, res) =>{
+    const user = new User(req.body)
+    //회원 가입할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+   await user
+   .save()
+      .then(() => {
+        res.status(200).json({
+          success: true,
+        });
+      })
+      .catch((err) => {
         console.error(err);
-        res.status(400).json({ success: false, err });
-    }
-});
+        res.json({
+          success: false,
+          err: err,
+        });
+      });
+  });
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
--- models/User.js
+++ models/User.js
@@ -1,35 +1,110 @@
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
     name:{
         type: String,
-        maxlength: 50
-    },
-    email: {
-        type: String,
-        trim:true,
-        unique: 1
-    },
-    lastname: {
-        type:String,
         maxlength:50
     },
-    role:{
-        type: Number,
-        default: 0
+    email: {
+        type:String,
+        trim:true,
+        unique:1
+    },
+    password: {
+        type:String, 
+        minlength: 5
+    },
+    lastname: {
+        type:String, 
+        maxlength: 50
+    },
+    role: {
+        type:Number,
+        default:0
     },
     image: String,
-    token :{
-        type: String
+    token: {
+        type:String
     },
     tokenExp:{
-        type: Number
+        type:Number
     }
 })
 
 const User = mongoose.model('User', userSchema)
+module.exports= {User}
 
-module.exports = { User }
+
+
+
+
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt')
+// const saltRounds = 10 
+// const jwt = require('jsonwebtoken')
+
+
+// const userSchema = mongoose.Schema({
+//     name:{
+//         type: String,
+//         maxlength: 50
+//     },
+//     email: {
+//         type: String,
+//         trim:true,
+//         unique: 1
+//     },
+//     password: {
+//         type: String
+//     },
+//     lastname: {
+//         type: String,
+//         maxlength:50
+//     },
+//     role:{
+//         type: Number,
+//         default: 0
+//     },
+//     image: String,
+//     token :{
+//         type: String
+//     },
+//     tokenExp:{
+//         type: Number
+//     }
+// })
+
+
+// userSchema.pre('save', function( next ){
+//     // 비밀번호 암호화
+//     var user = this;
+//     if (!user.isModified('password')) return next();
+
+//     bcrypt.genSalt(saltRounds, function(err, salt) {
+//         if (err) return next(err);
+//         bcrypt.hash(user.password, salt, function(err, hash) {
+//         // returns hash
+//             if (err) return next(err);
+//             user.password = hash;
+//             next();
+//         });
+//       });
+// });
+
+// userSchema.methods.comparePassword = function(plainPassword) {
+//     return bcrypt.compare(plainPassword, this.password);
+// };
+
+// userSchema.methods.generateToken = function(cb) {
+//     var user = this;
+//     var token = jwt.sign(user._id.toHexString(), 'YOUR_SECRET_KEY');
+//     user.token = token;
+//     user.save(function(err, user) {
+//         if (err) return cb(err);
+//         cb(null, user);
+//     });
+// };
+
+// const User = mongoose.model('User', userSchema)
+
+// module.exports = { User }
